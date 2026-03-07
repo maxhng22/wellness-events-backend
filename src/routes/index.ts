36c55e9af718dab08449e63d84a956e936bb056f
Max Hng <maxkhuan1@gmail.com>
2026-03-07 23:10:50 +0800

    feature: added route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 // import healthRoutes from './health.routes';
 import userRoutes from './auth.routes';
+import eventItemRoutes from './event_item.route';
+import eventRoutes from './event.routes';
 
 const router = Router();
 
 // router.use('/health', healthRoutes);
 router.use('/auth', userRoutes);
+router.use('/event', eventRoutes);
+router.use('/event-items', eventItemRoutes);
 
 export default router;
